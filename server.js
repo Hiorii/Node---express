6ee64cf7e041b60b95a64ba817d9d2f1fa3a7ba1
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-12-01 19:57:26 +0100

    Add form and http request

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const hbs = require('express-handlebars');
 
-app.use((req,res,next)=>{
-    res.show = (name) => {
-        res.sendFile(path.join(__dirname, `/views/${name}`))
-    };
-    next();
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use('/user',(req,res,next) => {
    res.send('You have login to see this context');
@@ -15,25 +12,59 @@ app.use('/user',(req,res,next) => {
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/',(req,res)=>{
-    res.show('home.html');
+    res.render('home');
 });
 
 app.get('/about',(req,res)=>{
-    res.show('about.html');
+    res.render('about', {layout: 'dark'});
+});
+
+app.get('/contact',(req,res)=> {
+    res.render('contact');
+});
+
+app.get('/info',(req,res)=> {
+    res.render('info');
+});
+
+app.get('/history',(req,res)=> {
+    res.render('history');
 });
 
 app.get('/user/settings',(req,res)=>{
-    res.show('settings.html');
+    res.render('settings');
 });
 
 app.get('/user/panel',(req,res)=>{
-    res.show('panel.html');
+    res.render('panel');
+});
+
+app.get('/hello/:name',(req,res)=>{
+    res.render('hello', {name: req.params.name});
+});
+
+app.post('/contact/send-message', (req, res) => {
+    const { author, sender, title, file, message } = req.body;
+    const allowedExtensions = ['png', 'jpg', 'jpeg', 'gif'];
+    const fileExtension = file.split('.').pop().toLowerCase();
+
+    if(!author || !sender || !title || !file || !message) {
+        res.render('contact', {isError: true});
+    }else {
+        if(allowedExtensions.includes(fileExtension)) {
+            res.render('contact', {isSent: true, file: file});
+        }
+        else {
+            res.render('contact', { file: file, isValid: true });
+        }
+    }
 });
 
 app.use((req,res)=> {
-    res.status(404).show('error.html');
+    res.status(404).render('error');
 })
 
 app.listen(8000, ()=> {
